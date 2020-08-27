610377633e0959c860f6c5b0336c2cf62d4e0f88
Ruslan Lukmanov <dr_johnd@mail.ru>
2020-08-27 20:20:02 +0300

    v1

---
+++ WeatherAndro/Delegate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherAndro
+//
+//  Created by Ruslan Lukmanov on 27.08.2020.
+//  Copyright © 2020 Ruslan Lukmanov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
